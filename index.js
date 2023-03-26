cdf28098dc80c0ee5d6199413be004c97d2e80a5
nuru <nurudiin222@gmail.com>
2023-03-26 07:32:56 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards(namesArray, event) {
+ let messagesArray = [];
+ for (let i = 0; i < namesArray.length; i++) {
+   let message = `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`;
+   messagesArray.push(message);
+ }
+ return messagesArray;
+}
+
+function countDown(num) {
+ for (let i = num; i >= 0; i--) {
+   console.log(i);
+ }
+}
+
